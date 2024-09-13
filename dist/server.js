078bdf45c939c84702f16777c641d131ccd9de3b
Suresh Ramani <suresh@serveravatar.com>
2024-09-13 11:36:04 +0530

    Test

---
+++ dist/server.js
@@ -0,0 +1,15 @@
+// Import necessary modules
+const express = require('express');
+
+// Create an Express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, ServerAvatar Express!');
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
